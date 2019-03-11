556a5030648cd3237ecae194eb069a416a7c277d
Kevin-CodeCrew <kevin@code-crew.org>
2019-03-11 11:18:11 -0500

    Graded 03-11-2019 11:18:01

--- authentication2Project/authentication2App/models.py
+++ authentication2Project/authentication2App/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class BlogPosts(models.Model):
+	# KEY: Should probably use CASCADE delete option so bloggers posts get deleted if blogger deleted
     user_name = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True)
     blog_title = models.CharField(max_length=100)
     blog_entry = models.CharField(max_length=500)
